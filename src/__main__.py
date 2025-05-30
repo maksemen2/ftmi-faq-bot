f6930dce67d197a2fba41e8ffd2aae0953fffb5e
maksemen2 <lxshnaps@gmail.com>
2025-05-30 16:01:34 +0300

    add main entry point for Telegram bot with asynchronous execution

---
+++ src/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from config.telegram import TelegramSettings
+from delivery.telegram.factory import run_bot
+from infrastructure.DI.factory import create_dishka
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+async def main():
+    container = create_dishka()
+    
+    tg_settings = await container.get(TelegramSettings)
+    await run_bot(
+        container,
+        mod_chat_id=tg_settings.mod_channel,
+        bot_token=tg_settings.bot_token.get_secret_value(),
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
